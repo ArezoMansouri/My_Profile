77a036a170835d6d2ac754ac95535defbcc8d666
ArezoMansouri <arezomansouri2000@gmail.com>
2025-11-08 20:25:51 +0430

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFFFC1DC),
+        body: Container(),
+      ),
+    ),
+  );
+}
